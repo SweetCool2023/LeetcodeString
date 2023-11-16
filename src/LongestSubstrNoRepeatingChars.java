6f4632baf717dcf599fd29d4fd992fa9010890be
SweetCool2023 <xufen_gao@yahoo.com>
2023-11-16 08:58:02 -0800

    1st check in

---
+++ src/LongestSubstrNoRepeatingChars.java
@@ -0,0 +1,11 @@
+
+public class LongestSubstrNoRepeatingChars {
+	public String longestStrNonRepeatingChars(String str) {
+		String result = "";
+		
+		System.out.println("Hello World");
+		
+		return result;
+	}
+
+}
---
+++ src/dtiver.java
@@ -0,0 +1,12 @@
+
+public class dtiver {
+
+	public static void main(String[] args) {
+		// 3. Longest Substring Without Repeating Characters
+        String string1 = "abcabcbb";
+		LongestSubstrNoRepeatingChars longestSubstrNoRepeatingChars = new LongestSubstrNoRepeatingChars();
+		longestSubstrNoRepeatingChars.longestStrNonRepeatingChars(string1);
+
+	}
+
+}
